f7aefecf9d554d248fd55749fc595896a62c3ac2
sheetanshu-limitinfinity <66507027+sheetanshu-limitinfinity@users.noreply.github.com>
2022-09-01 13:15:37 +0530

    many

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,11 +2,28 @@ import React from "react";
 import { View, Image, Text, StyleSheet } from "react-native";
 
 const ImageDetail = ({ name, image }) => {
+  console.log(image);
   return (
     <View>
+      <Image style={imageDiv} source={image} />
       <Text>Hello {name}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  viewDiv: {
+    flex: 1,
+    flexDirection: "row",
+    maxWidth: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageDiv: {
+    width: "100%",
+  },
+});
+
+const { imageDiv } = styles;
 
 export default ImageDetail;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -6,23 +6,19 @@ import ImageDetail from "../components/ImageDetail";
 const images = [
   {
     name: "Rand One",
-    image:
-      "https://i.pinimg.com/736x/39/6c/0f/396c0fe77fc94c3f0199ec8243421662.jpg",
+    image: require("../../assets/396c0fe77fc94c3f0199ec8243421662.jpeg"),
   },
   {
     name: "Rand Two",
-    image:
-      "https://www.scrolldroll.com/wp-content/uploads/2022/05/Saiee-Manjrekar-Youngest-Bollywood-Actresses.jpeg",
+    image: require("../../assets/Saiee-Manjrekar-Youngest-Bollywood-Actresses.jpeg"),
   },
   {
     name: "Rand Three",
-    image:
-      "https://www.pinkvilla.com/files/styles/amp_metadata_content_image_min_696px_wide/public/bollywood-actresses-in-saree.jpg",
+    image: require("../../assets/bollywood-actresses-in-saree.jpeg"),
   },
   {
     name: "Rand Four",
-    image:
-      "https://stylesatlife.com/wp-content/uploads/2022/02/B-Town-Actress-Janhvi-Kapoor.jpg.webp",
+    image: require("../../assets/B-Town-Actress-Janhvi-Kapoor.jpg.jpeg"),
   },
 ];
 
